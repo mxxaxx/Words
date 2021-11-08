cda2c975e98497cc26cccf4a0b7d8ba7db6de701
mxxaxx <malikbreitbach@icloud.com>
2021-11-08 07:06:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,119 @@
+var wordDict = {"Acquiesce":"This word means that a person has agreed or assented either verbally or tacitly to something.",
+"This word means that a person has agreed or assented either verbally or tacitly to something.":"Acquiesce",
+"Aberration":"Sometimes, it’s good to be different, but the word “aberration” describes an unwelcome oddity. So, if someone accuses you of aberrant behavior, they aren’t complimenting your originality.",
+"Sometimes, it’s good to be different, but the word “aberration” describes an unwelcome oddity. So, if someone accuses you of aberrant behavior, they aren’t complimenting your originality.":"Abberation",
+"Abjure":"If you solemnly renounce something, you have abjured it. The word is used in baptism ceremonies in certain churches. The person being baptized declares that he or she abjures Satan and all his works.",
+"If you solemnly renounce something, you have abjured it. The word is used in baptism ceremonies in certain churches. The person being baptized declares that he or she abjures Satan and all his works.":"Abjure",
+"Abrogate":"Abrogation is a situation in which formal or legal measures are taken to do away with something. This would usually be a law or a formal rule that is repealed or temporarily suspended.",
+"Abrogation is a situation in which formal or legal measures are taken to do away with something. This would usually be a law or a formal rule that is repealed or temporarily suspended.":"Abrogate",
+"Anachronism":"An anachronism is something that is out of date and no longer relevant to the times. If you want to argue that something has lost its relevance by not changing with the times, you can describe it as being anachronistic or an anachronism.",
+"An anachronism is something that is out of date and no longer relevant to the times. If you want to argue that something has lost its relevance by not changing with the times, you can describe it as being anachronistic or an anachronism.":"Anachronism",
+"Anathema":"It describes something that you vehemently dislike to the point of total rejection.",
+"It describes something that you vehemently dislike to the point of total rejection.":"Anathema",
+"Antebellum":"A thing that is described as “antebellum” was conceived or constructed prior to an important war.",
+"A thing that is described as “antebellum” was conceived or constructed prior to an important war.":"Antebellum",
+"Anthropogenic":"Anything that has been caused by human interventions is anthropogenic.",
+"Anything that has been caused by human interventions is anthropogenic.":"Anthropogenic",
+"Antithesis":"Something or someone that is the diametric opposite of something else. “He was the antithesis of the frivolous millionaire playboy,” would imply that the person was the opposite of what one expect from a happy-go-lucky millionaire. Perhaps he was very serious about current issues, or didn’t like wasting money on status symbols.",
+"Something or someone that is the diametric opposite of something else. “He was the antithesis of the frivolous millionaire playboy,” would imply that the person was the opposite of what one expect from a happy-go-lucky millionaire. Perhaps he was very serious about current issues, or didn’t like wasting money on status symbols.":"Antithesis",
+"Bellicose":"This is a lovely word for describing people who are aggressive and even willing to fight over an issue.",
+"This is a lovely word for describing people who are aggressive and even willing to fight over an issue.":"Bellicose",
+"Chicanery":"“Chicanery” alludes to the use of dirty tricks in the financial, political or legal world. If you are ever accused of it, know that you are in deep trouble!",
+"“Chicanery” alludes to the use of dirty tricks in the financial, political or legal world. If you are ever accused of it, know that you are in deep trouble!":"Chicanery",
+"Chthonic":"It refers to caves or the underworld.",
+"It refers to caves or the underworld.":"Chthonic",
+"Cerulean":"You will no doubt have read about cerulean waters or skies and assumed that they were blue. You are right. Specifically, the word refers to a deep, sky-blue.",
+"You will no doubt have read about cerulean waters or skies and assumed that they were blue. You are right. Specifically, the word refers to a deep, sky-blue.":"Cerulean",
+"Circumspect":"If you are behaving with circumspection, you are watchful, wary or unwilling to take risks.",
+"If you are behaving with circumspection, you are watchful, wary or unwilling to take risks.":"Circumspect",
+"Circumlocution":"When someone “talks around” a subject instead of being specific and concise",
+"When someone “talks around” a subject instead of being specific and concise":"Circumlocution",
+"Cogent":"The person who is guilty of circumlocution, should try to be more cogent. In other words, they should be clear, logical and convincing.",
+"The person who is guilty of circumlocution, should try to be more cogent. In other words, they should be clear, logical and convincing.":"Cogent",
+"Colloquial":"Informal Language",
+"Informal Language": "Colloquial",
+"Crepuscular": "This lovely word is used to describe things that resemble or relate to twilight. In its simplest form, it is used to describe creatures that become active at twilight, but I’m sure you know a few people like that too!",
+"This lovely word is used to describe things that resemble or relate to twilight. In its simplest form, it is used to describe creatures that become active at twilight, but I’m sure you know a few people like that too!":"Crepuscular",
+"Deleterious":"If something has harmful effects, they can be described as “deleterious effects.”",
+"If something has harmful effects, they can be described as “deleterious effects.”":"Deleterious",
+"Depredation":"an act of attack or plunder",
+"an act of attack or plunder": "Depredation",
+"Didactic":"In its simplest context, this word describes something that was designed to teach a lesson. For example, a didactic story would have a strong lesson to teach. However, the word can also be used negatively to describe someone who is preachy and patronizing.",
+"In its simplest context, this word describes something that was designed to teach a lesson. For example, a didactic story would have a strong lesson to teach. However, the word can also be used negatively to describe someone who is preachy and patronizing.": "Didactic",
+"Egregious":"Terriby bad or shocking",
+"Terriby bad or shocking":"Egregious",
+"Enervate":"If you are feeling drained, weakened and tired out, you have probably been through some sort of enervating experience. There are good types of tiredness, but this is not one of them.",
+"If you are feeling drained, weakened and tired out, you have probably been through some sort of enervating experience. There are good types of tiredness, but this is not one of them.":"Enervate",
+"Entomophagy":"The eating of insects",
+"The eating of insects":"Entomophagy",
+"Epiphany":"“Aha!” moment",
+"“Aha!” moment":"Epiphany",
+"Eschatology":"It is a religious doctrine that deals with death, judgement, and what happens to the soul after death.",
+"It is a religious doctrine that deals with death, judgement, and what happens to the soul after death.":"Eschatology",
+"Eschew": "Here’s a useful little word that says more than it seems to. For example, you could say someone doesn’t exercise, but if you say, “He eschewed exercise,” it implies a stronger and more deliberate aversion.",
+"Here’s a useful little word that says more than it seems to. For example, you could say someone doesn’t exercise, but if you say, “He eschewed exercise,” it implies a stronger and more deliberate aversion.":"Eschew",
+"Evanescent":"Evanescent refers to something that is transitory, or that disappears or fades rapidly.",
+"Evanescent refers to something that is transitory, or that disappears or fades rapidly.":"Evanescent",
+"Facetious":"Facetious means making inappropriate jokes or taking serious situations too lightly.",
+"Facetious means making inappropriate jokes or taking serious situations too lightly.":"Facetious",
+"Fatuous":"A fatuous comment is silly, inane, or just plain stupid. Save this remark for someone who really deserves it, and serve it up cold.",
+"A fatuous comment is silly, inane, or just plain stupid. Save this remark for someone who really deserves it, and serve it up cold.":"Fatuous",
+"Fiduciary":"It is the term used to describe a trustee who takes care of assets on behalf of one or more beneficiaries.",
+"It is the term used to describe a trustee who takes care of assets on behalf of one or more beneficiaries.":"Fiduciary",
+"Filibuster":"The filibuster is a person who uses a dirty, time-wasting trick to hold up a legislative decision. Without breaking any rules, the filibuster speaks at great length without saying anything useful.",
+"The filibuster is a person who uses a dirty, time-wasting trick to hold up a legislative decision. Without breaking any rules, the filibuster speaks at great length without saying anything useful.":"Filibuster",
+"Fulminate":"Let’s use an example: your teacher tells the class that the holiday assignment is a 10,000-word essay. After class, everyone discusses how furious they are, how unfair it is, and so on. They are, in fact, fulminating.",
+"Let’s use an example: your teacher tells the class that the holiday assignment is a 10,000-word essay. After class, everyone discusses how furious they are, how unfair it is, and so on. They are, in fact, fulminating.":"Fulminate",
+"Hegemony":"When one group politically dominates all the others, it is called a “hegemony,” and the term can be applied to any form of government that fits the description.",
+"When one group politically dominates all the others, it is called a “hegemony,” and the term can be applied to any form of government that fits the description.":"Hegemony",
+"Homonym":"When two words are spelled the same, are pronounced the same, but have different meanings, they are homonyms.",
+"When two words are spelled the same, are pronounced the same, but have different meanings, they are homonyms.":"Homonym",
+"Hubris":"Hubris means excessive pride or confidence that could lead to terrible consequences.",
+"Hubris means excessive pride or confidence that could lead to terrible consequences.":"Hubris",
+"Inculcate":"This means teaching someone a principle or habit in such a way that the lesson is fully ingrained and adopted.",
+"This means teaching someone a principle or habit in such a way that the lesson is fully ingrained and adopted.":"Inculcate",
+"Juxtaposition":"When two contrasting situations or thoughts are compared for effect, we have a juxtaposition.",
+"When two contrasting situations or thoughts are compared for effect, we have a juxtaposition.":"Juxtaposition",
+"Jejune":"If someone presents you with a naïve point of view, seems to be oversimplifying, or has only superficial knowledge, you can use this word, both to describe their efforts, and to baffle them.",
+"If someone presents you with a naïve point of view, seems to be oversimplifying, or has only superficial knowledge, you can use this word, both to describe their efforts, and to baffle them.":"Jejune",
+"Lionize":"When you treat someone as if they were a celebrity, then you are lionizing them.",
+"When you treat someone as if they were a celebrity, then you are lionizing them.":"Lionize",
+"Lucubration":"The word “lucubration” could be used to indicate something that has been given a lot of study and deep thought, but it’s also a rather rude way of describing a piece of writing that seems terribly pedantic and overelaborate.",
+"The word “lucubration” could be used to indicate something that has been given a lot of study and deep thought, but it’s also a rather rude way of describing a piece of writing that seems terribly pedantic and overelaborate.":"Lucubration",
+"Magnanimous":"When someone is very generous or forgiving to someone in a less powerful positon, you can call him or her “magnanimous.”",
+"When someone is very generous or forgiving to someone in a less powerful positon, you can call him or her “magnanimous.”":"Magnanimous",
+"Moiety":"In anthropology, this term relates to the groups into which people are divided during important rituals, but it has also come to mean a share, particularly a lesser one.",
+"In anthropology, this term relates to the groups into which people are divided during important rituals, but it has also come to mean a share, particularly a lesser one.":"Moiety",
+"Nihilism":"“Nothing matters, everything is trivial, even existence is questionable and could be an illusion. There is no God, and nothing has any importance.” If this depressing philosophy appeals to you, you are a nihilist.",
+"“Nothing matters, everything is trivial, even existence is questionable and could be an illusion. There is no God, and nothing has any importance.” If this depressing philosophy appeals to you, you are a nihilist.":"Nihilism",
+"Nomenclature":"A nomenclature is a system designed for naming things. It could be a set of terminology, a term, or it could refer to a scientific naming system such as that used to identify all living things with two Latin names.",
+"A nomenclature is a system designed for naming things. It could be a set of terminology, a term, or it could refer to a scientific naming system such as that used to identify all living things with two Latin names.":"Nomenclature",
+"Paucity":"When there is too little of anything, you are suffering a paucity of it. It’s most often used in relation to facts, but it works just as well and even more uncomfortably with funds.",
+"When there is too little of anything, you are suffering a paucity of it. It’s most often used in relation to facts, but it works just as well and even more uncomfortably with funds.":"Paucity"
+
+}
+function getdefinition(){
+    var word = document.getElementById("word").innerText
+
+    document.getElementById("word").textContent=wordDict[word]
+    if (document.getElementById("word").style.fontSize=="3vh"){
+        document.getElementById("word").style.fontSize = "5vh"
+    }
+    else{
+        document.getElementById("word").style.fontSize = "3vh"
+    }
+}
+var wordIndex = 0;
+const words = ["Acquiesce", "Aberration", "Abjure", "Abrogate", "Anachronism", "Anathema", "Antebellum", "Anthropogenic", "Antithesis", "Bellicose", "Chicanery", "Chthonic",
+"Cerulean", "Circumspect", "Circumlocution", "Cogent", "Colloquial", "Crepuscular", "Deleterious", "Depredation", "Didactic", "Egregious", "Enervate", "Entomophagy", "Epiphany",
+"Eschatology","Eschatology", "Eschew", "Evanescent", "Fatuous", "Fiduciary", "Filibuster", "Fulminate","Hegemony","Homonym","Hubris","Inculcate","Juxtaposition","Jejune",
+"Lionize","Lucubration", "Magnanimous","Moiety","Nihilism","Nomenclature","Paucity"
+]
+function nextword(){
+    wordIndex=wordIndex+1
+
+    document.getElementById("word").textContent=words[wordIndex]
+    setTimeout(nextword, 1000)
+
+}
+setTimeout(nextword, 1000)
