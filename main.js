1e57f2acd1dfcc40b2c43abecf28bd55e4e1db73
mxxaxx <67272851+mxxaxx@users.noreply.github.com>
2021-11-10 16:03:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -115,4 +115,4 @@ function nextword(){
     document.getElementById("word").textContent=words[wordIndex]
 
 }
-var abcdefghij = setInterval(nextword, 86400000)
+var abcdefghij = setInterval(nextword, 10000)
